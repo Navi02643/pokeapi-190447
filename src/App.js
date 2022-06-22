d8c03a7691ae4340952a7a5c6d9e434a0a7bf267
Navi026 <karlos123jun@gmail.com>
2022-06-21 20:01:58 -0500

    feat: pokeapi finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
-
+/* eslint-disable */
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 function App() {
+  const [pokemon, setPokeNum] = useState(0);
+  const [pokedata, setPokeData] = useState("");
+  const [pokedataesp, setPokeDataESP] = useState([]);
+
+  const getdata = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${pokemon}`)
+      .then((res) => {
+        setPokeData(res.data.results);
+      });
+  };
+
+  useEffect(getdata, [pokemon]);
+
+  useEffect(() => {
+    setPokeDataESP([]);
+    if (pokedata.length >= 1) {
+      pokedata.map((item) => {
+        axios.get(`${item.url}`).then((data) => {
+          setPokeDataESP((current) => [...current, { data: data.data }]);
+          if (pokedataesp >= 10) {
+            return pokedataesp;
+          }
+        });
+      });
+    }
+  }, [pokedata]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-5">
+      <input
+        className="btn btn-outline-success"
+        type="button"
+        value="Pagina Siguiente"
+        onClick={(event) => {
+          if(pokemon>=1126){
+            setPokeNum(0);
+          }else {
+            setPokeNum(Number(pokemon) + 10);
+            console.log(pokemon);
+          }
+        }}
+      />
+      <input
+        className="btn btn-outline-success"
+        type="button"
+        value="Pagina Anterior"
+        onClick={(event) => {
+          if (pokemon <= 0) {
+            setPokeNum(Number(1116));
+          } else {
+            setPokeNum(Number(pokemon) - 10);
+          }
+        }}
+      />
+      <hr />
+      <table className="table table-secondary table-striped">
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Sprite Front</td>
+            <td>Sprite Back</td>
+            <td>Sprite Shini Front</td>
+            <td>Sprite Shini Back</td>
+          </tr>
+        </thead>
+        <tbody>
+          {pokedataesp.map((poke, index) => (
+            <tr key={index}>
+              <td>{`${poke.data.id}`}</td>
+              <td>{`${poke.data.name}`}</td>
+              <td>
+                <img src={poke.data.sprites.front_default}></img>
+              </td>
+              <td>
+                <img src={poke.data.sprites.back_default}></img>
+              </td>
+              <td>
+                <img src={poke.data.sprites.front_shiny}></img>
+              </td>
+              <td>
+                <img src={poke.data.sprites.back_shiny}></img>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
